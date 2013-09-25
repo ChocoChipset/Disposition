afd66cb210a3b615fcb76ca5f9d212d1296247ea
Hector Zarate <hecktorzr@gmail.com>
2013-09-25 20:02:58 +0200

    Added CGRect+Disposition header file

---
+++ Source/CGRect+Disposition.h
@@ -0,0 +1,15 @@
+//
+//  CGRect+Disposition.h
+//  DispositionUnitTests
+//
+//  Created by Hector Zarate on 9/25/13.
+//  Copyright (c) 2013 Hector Zarate. All rights reserved.
+//
+
+#ifndef DispositionUnitTests_CGRect_Disposition_h
+#define DispositionUnitTests_CGRect_Disposition_h
+
+
+
+
+#endif
---
+++ Source/CGRect+Disposition.m
@@ -0,0 +1,9 @@
+//
+//  CGRect+Disposition.c
+//  DispositionUnitTests
+//
+//  Created by Hector Zarate on 9/25/13.
+//  Copyright (c) 2013 Hector Zarate. All rights reserved.
+//
+
+#include <stdio.h>
