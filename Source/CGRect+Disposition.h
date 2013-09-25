8cab7e72ce73cd4cf93c25de036e4ab8817e1995
Hector Zarate <hecktorzr@gmail.com>
2013-09-25 20:34:00 +0200

    Implemented first additional functions for CGRect structure

--- Source/CGRect+Disposition.h
+++ Source/CGRect+Disposition.h
@@ -6,10 +6,57 @@
 //  Copyright (c) 2013 Hector Zarate. All rights reserved.
 //
 
-#ifndef DispositionUnitTests_CGRect_Disposition_h
-#define DispositionUnitTests_CGRect_Disposition_h
+#include <CoreGraphics/CGBase.h>
+#include <CoreFoundation/CFDictionary.h>
+
+#pragma mark - Headers
+
+static inline void CGRectSetSize(CGRect *rect, CGSize size);
+static inline void CGRectSetWidth(CGRect *rect, CGFloat width);
+static inline void CGRectSetHeight(CGRect *rect, CGFloat height);
+
+static inline void CGRectSetOrigin(CGRect *rect, CGPoint originPoint);
+static inline void CGRectSetX(CGRect *rect, CGFloat x);
+static inline void CGRectSetY(CGRect *rect, CGFloat y);
+
+#pragma mark - Implementation: Size
 
 
+static inline void CGRectSetSize(CGRect *rect, CGSize size)
+{
+    (*rect).size.height = size.height;
+    (*rect).size.width = size.width;
+}
+
+static inline void CGRectSetWidth(CGRect *rect, CGFloat width)
+{
+    (*rect).size.width = width;
+}
 
 
-#endif
+static inline void CGRectSetHeight(CGRect *rect, CGFloat height)
+{
+    (*rect).size.height = height;
+}
+
+
+#pragma mark - Implementation: Origin
+
+
+static inline void CGRectSetOrigin(CGRect *rect, CGPoint originPoint)
+{
+    (*rect).origin.x = originPoint.x;
+    (*rect).origin.y = originPoint.y;
+}
+
+
+static inline void CGRectSetX(CGRect *rect, CGFloat x)
+{
+    (*rect).origin.x = x;
+}
+
+
+static inline void CGRectSetY(CGRect *rect, CGFloat y)
+{
+    (*rect).origin.y = y;
+}
--- Source/CGRect+Disposition.m
+++
@@ -1,9 +0,0 @@
-//
-//  CGRect+Disposition.c
-//  DispositionUnitTests
-//
-//  Created by Hector Zarate on 9/25/13.
-//  Copyright (c) 2013 Hector Zarate. All rights reserved.
-//
-
-#include <stdio.h>
